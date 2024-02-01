d160b9d4b5529d4e33fdef70381fff0738f772c7
mcsnette <126247297+mcsnette@users.noreply.github.com>
2024-02-01 23:44:45 +0800

    deleted detail and popup

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Glasses from "./Glasses";
-import Popup from "./Popup";
-import Details from "./Details";
 import Footer from "./Footer";
 
 function App() {
--- src/Popup.js
+++
@@ -1,9 +0,0 @@
-function Popup() {
-  return (
-    <div className="header">
-      <img src="./images/popup.jpg" />
-    </div>
-  );
-}
-
-export default Popup;
